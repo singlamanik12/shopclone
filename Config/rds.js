e29b0bbcc28655e8adb06225c6eacf62b8a21543
singlamanik12 <44194522+singlamanik12@users.noreply.github.com>
2021-01-26 19:59:51 -0500

    first

--- Config/rds.js
+++ Config/rds.js
@@ -1,13 +1,15 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'mydatabase.ci1zqmbyhspo.ap-south-1.rds.amazonaws.com',
-    user     : 'anshul',
-    password : 'anshul1121',
-    database : 'mydatabase'
-});
+var mysql = require("mysql");
+var connection = mysql.createConnection(process.env.DATABASE_URL);
 
-connection.connect(function(err) {
-    if (err) throw err;
+connection.connect(function (err) {
+  if (err) throw err;
 });
 
 module.exports = connection;
+
+// {
+//     host     : 'mydatabase.ci1zqmbyhspo.ap-south-1.rds.amazonaws.com',
+//     user     : 'anshul',
+//     password : 'anshul1121',
+//     database : 'mydatabase'
+// }
